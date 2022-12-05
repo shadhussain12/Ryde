e74fb158dbd67134fe1945f9d4c97d1342f6950d
Shad Hussain <shad12@outlook.com>
2022-12-04 22:56:24 -0800

    data not displaying

--- ryde-app/src/App.js
+++ ryde-app/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(true);
+  const [loading, setLoading] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`https://dev-backend.rydecarpool.com/coding-challenge/signups?target=dev`);
+        console.log(response);
+        setData(response.data);
+        setError(null);
+      } 
+      catch (err) {
+        setError(err.message);
+        setData(null);
+      }
+      finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis dataKey="count"/>
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Signups" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
   );
 }
 
