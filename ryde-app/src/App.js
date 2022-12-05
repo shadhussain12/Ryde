337526915e0c86f3e3336f2c1c3468f2e0caef2a
Shad Hussain <shad12@outlook.com>
2022-12-05 12:51:14 -0800

    done

--- ryde-app/src/App.js
+++ ryde-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import {useEffect, useState} from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Brush } from 'recharts';
 import axios from "axios";
+import moment from "moment";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const [error, setError] = useState(true);
   const [loading, setLoading] = useState(null);
 
@@ -15,7 +16,7 @@ function App() {
         console.log(response);
         setData(response.data);
         setError(null);
-      } 
+      }
       catch (err) {
         setError(err.message);
         setData(null);
@@ -27,27 +28,67 @@ function App() {
     getData();
   }, []);
 
+  const CustomizedAxisTick = ({ x, y, payload }) => {
+    const dateTip = moment(payload.value)
+      .format("ll")
+      .slice(0, 6);
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={23} y={0} dy={14} fontSize="0.90em" fontFamily="bold" textAnchor="end" fill="#fff">
+          {dateTip}</text>
+      </g>
+    );
+  }
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    const dateTip = moment(label)
+      .format("llll")
+      .slice(0, 12);
+    const formattedDate = dateTip
+    if (payload === null) return
+    if (active)
+      return (
+        <div className="custom-tooltip">
+          <p className="label-tooltip">{`${formattedDate}`}</p>
+          <p className="desc-tooltip">
+            <span className="value-tooltip">Total Signups: {payload[0].value.toLocaleString()}</span>
+          </p>
+        </div>
+      );
+    return null;
+  };
+
+  const xAxisTickFormatter = (timestamp_measured) => {
+    return moment(timestamp_measured)
+      .format("ll")
+      .slice(0, 6);
+  }
+
   return (
-    <ResponsiveContainer width="100%" aspect={3}>
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis dataKey="count"/>
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="Signups" stroke="#8884d8" activeDot={{ r: 8 }} />
-        </LineChart>
-      </ResponsiveContainer>
+    <>
+    <h2>Ryde User Signups Per Day 2022</h2>
+    <ResponsiveContainer width="90%" aspect={3}>
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 0,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" horizontal="true" vertical="" />
+        <XAxis dataKey="date" tick={CustomizedAxisTick}/>
+        <YAxis dataKey="count"/>
+        <Tooltip content={<CustomTooltip />} animationDuration={0} />
+        <Legend />
+        <Brush tickFormatter={xAxisTickFormatter} dataKey="date" color="gray" />
+        <Line type="monotone" dataKey="count" stroke="#8884d8" dot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 2, r: 2 }} activeDot={{ r: 6 }} />
+      </LineChart>
+    </ResponsiveContainer>
+    </>
   );
 }
 
